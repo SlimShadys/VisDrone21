9e333968cf6f69d86a47dfacae0bd7f32a00ea13
Gianmarco Scarano <gianmarcoscarano@gmail.com>
2021-12-14 12:57:40 +0100

    VisDrone21GPU: Remove useless variables in test.py
    Signed-off-by: Gianmarco Scarano <gianmarcoscarano@gmail.com>

--- TransCrowd/test.py
+++ TransCrowd/test.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 from torchvision import transforms
 import dataset
 import math
-from utils import save_checkpoint, setup_seed
+from utils import setup_seed
 import torch
 import os
 import logging
@@ -13,46 +13,52 @@ import nni
 from nni.utils import merge_parameter
 from config import return_args, args
 import numpy as np
+from tqdm import trange
 from image import load_data
 
 warnings.filterwarnings('ignore')
-import time
 
 setup_seed(args.seed)
 
 logger = logging.getLogger('mnist_AutoML')
 
-
 def main(args):
     if args['dataset'] == 'ShanghaiA':
-        train_file = './npydata/ShanghaiA_train.npy'
         test_file = './npydata/ShanghaiA_test.npy'
     elif args['dataset'] == 'ShanghaiB':
-        train_file = './npydata/ShanghaiB_train.npy'
         test_file = './npydata/ShanghaiB_test.npy'
     elif args['dataset'] == 'UCF_QNRF':
-        train_file = './npydata/qnrf_train.npy'
         test_file = './npydata/qnrf_test.npy'
     elif args['dataset'] == 'JHU':
-        train_file = './npydata/jhu_train.npy'
         test_file = './npydata/jhu_val.npy'
     elif args['dataset'] == 'NWPU':
-        train_file = './npydata/nwpu_train.npy'
         test_file = './npydata/nwpu_val.npy'
+    elif args['dataset'] == 'VisDrone':
+        test_file = './npydata/visDrone_test.npy'
 
-    with open(train_file, 'rb') as outfile:
-        train_list = np.load(outfile).tolist()
+    ''' For testing in Spyder '''
+    #args['pre'] = './save_file/VisDrone/model_best.pth'
+    #args['batch_size'] = 8
+    #args['epochs'] = 50
+    #args['dataset'] = 'VisDrone'
+    #test_file = './npydata/visDrone_test.npy'
+    
     with open(test_file, 'rb') as outfile:
         val_list = np.load(outfile).tolist()
 
-    print(len(train_list), len(val_list))
+    print("Lenght Test list: " + str(len(val_list)))
+
+    try:
+        print('*-----------------------------------------*')
+        print('Cuda available: {}'.format(torch.cuda.is_available()))
+        print("GPU: " + torch.cuda.get_device_name(torch.cuda.current_device()))
+        print('*-----------------------------------------*')
+    except:
+        pass
 
     os.environ['CUDA_VISIBLE_DEVICES'] = args['gpu_id']
 
-    if args['model_type'] == 'token':
-        model = base_patch16_384_token(pretrained=True)
-    elif args['model_type'] == 'gap':
-        model = base_patch16_384_gap(pretrained=True)
+    model = base_patch16_384_token(pretrained=True)
 
     model = nn.DataParallel(model, device_ids=[0])
     model = model.cuda()
@@ -93,16 +99,12 @@ def main(args):
 
     print(' * best MAE {mae:.3f} '.format(mae=args['best_pred']))
 
-
-
-
-
-def pre_data(train_list, args, train):
+def pre_data(val_list, args, train):
     print("Pre_load dataset ......")
     data_keys = {}
     count = 0
-    for j in range(len(train_list)):
-        Img_path = train_list[j]
+    for j in trange(len(val_list)):
+        Img_path = val_list[j]
         fname = os.path.basename(Img_path)
         img, gt_count = load_data(Img_path, args, train)
 
@@ -118,70 +120,11 @@ def pre_data(train_list, args, train):
         #     break
     return data_keys
 
-
-def train(Pre_data, model, criterion, optimizer, epoch, args, scheduler):
-    losses = AverageMeter()
-    batch_time = AverageMeter()
-    data_time = AverageMeter()
-
-    train_loader = torch.utils.data.DataLoader(
-        dataset.listDataset(Pre_data, args['save_path'],
-                            shuffle=True,
-                            transform=transforms.Compose([
-                                transforms.ToTensor(),
-
-                                transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                     std=[0.229, 0.224, 0.225]),
-                            ]),
-                            train=True,
-                            batch_size=args['batch_size'],
-                            num_workers=args['workers'],
-                            args=args),
-        batch_size=args['batch_size'], drop_last=False)
-    args['lr'] = optimizer.param_groups[0]['lr']
-    print('epoch %d, processed %d samples, lr %.10f' % (epoch, epoch * len(train_loader.dataset), args['lr']))
-
-    model.train()
-    end = time.time()
-
-    for i, (fname, img, gt_count) in enumerate(train_loader):
-
-        data_time.update(time.time() - end)
-        img = img.cuda()
-
-        out1 = model(img)
-        gt_count = gt_count.type(torch.FloatTensor).cuda().unsqueeze(1)
-
-        # print(out1.shape, kpoint.shape)
-        loss = criterion(out1, gt_count)
-
-        losses.update(loss.item(), img.size(0))
-        optimizer.zero_grad()
-
-        loss.backward()
-
-        optimizer.step()
-
-        batch_time.update(time.time() - end)
-        end = time.time()
-
-        if i % args['print_freq'] == 0:
-            print('4_Epoch: [{0}][{1}/{2}]\t'
-                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                .format(
-                epoch, i, len(train_loader), batch_time=batch_time,
-                data_time=data_time, loss=losses))
-
-    scheduler.step()
-
-
-def validate(Pre_data, model, args):
+def validate(test_data, model, args):
     print('begin test')
     batch_size = 1
     test_loader = torch.utils.data.DataLoader(
-        dataset.listDataset(Pre_data, args['save_path'],
+        dataset.listDataset(test_data, args['save_path'],
                             shuffle=False,
                             transform=transforms.Compose([
                                 transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406],
@@ -195,8 +138,6 @@ def validate(Pre_data, model, args):
 
     mae = 0.0
     mse = 0.0
-    visi = []
-    index = 0
 
     for i, (fname, img, gt_count) in enumerate(test_loader):
 
@@ -211,11 +152,12 @@ def validate(Pre_data, model, args):
             count = torch.sum(out1).item()
 
         gt_count = torch.sum(gt_count).item()
+                
         mae += abs(gt_count - count)
         mse += abs(gt_count - count) * abs(gt_count - count)
 
         if i % 15 == 0:
-            print('{fname} Gt {gt:.2f} Pred {pred}'.format(fname=fname[0], gt=gt_count, pred=count))
+            print('\n{fname}:\n- Gt {gt:.2f} - Pred {pred}'.format(fname=fname[0], gt=gt_count, pred=count))
 
     mae = mae * 1.0 / (len(test_loader) * batch_size)
     mse = math.sqrt(mse / (len(test_loader)) * batch_size)
